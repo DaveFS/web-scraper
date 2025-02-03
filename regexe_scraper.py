cfeb768d44d8522ce8404000e661cce6f8e949d7
DaveFS <david.william.gregory@gmail.com>
2025-02-03 06:44:41 +0000

    recommitting to progress

--- regexe_scraper.py
+++ regexe_scraper.py
@@ -5,7 +5,8 @@ import re
 
 #url = "https://www.bbc.co.uk/news/articles/cwyv8y68e25o" #bbc news article page
 #url = "http://olympus.realpython.org/profiles/aphrodite" # tutorial site
-url = "http://olympus.realpython.org/profiles/poseidon" # tutorial site - causes error with simple positional extract because of blank space.
+#url = "http://olympus.realpython.org/profiles/poseidon" # tutorial site - causes error with simple positional extract because of blank space.
+url = "http://olympus.realpython.org/profiles/dionysus"
 
 page = urlopen(url)
 
@@ -18,3 +19,7 @@ end_index = html.find("</title>")
 title = html[start_index:end_index]
 
 print(title)
+
+#re.findall("pattern","string",optional 3rd) - locates pattern in indicated string, 3rd argument can be applied e.g. IGNORECASE
+#re.search("pattern","string",optional 3rd) - every possible result, even within other matches
+#re.sub("pattern","replacement","string") - replaces everything in the indicated match with the 2nd argument
